d4c56909bebdb85476785a5cfee46282232a62d8
Atul Patil <atuluttampatil.1757@gmail.com>
2022-02-26 23:47:21 +0530

    🛠: add wallet connect function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,73 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './styles/App.css';
 
 
 
 const App = () => {
+
+	const [currentAccount, setCurrentAccount] = useState('');
+
+	const _tld = '.pns';
+	const CONTRACT_ADDRESS = '0xEf199B06A8dFca68bB9606F2E14fe7e072A950DF';
+
+	const [domain, setDomain] = useState('');
+	const [record, setRecord] = useState('');
+
+
+	const checkIfWalletIsConnected = async () => {
+		const { ethereum } = window;
+
+		if (!ethereum) {
+			console.log('Make sure you have metamask!');
+			return;
+		} else {
+			console.log('We have the ethereum object', ethereum);
+		}
+
+		const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+		if (accounts.length !== 0) {
+			const account = accounts[0];
+			console.log('Found an authorized account:', account);
+			setCurrentAccount(account);
+		} else {
+			console.log('No authorized account found');
+		}
+	};
+
+	const connectWallet = async () => {
+		try {
+			const { ethereum } = window;
+
+			if (!ethereum) {
+				alert("Get MetaMask -> https://metamask.io/");
+				return;
+			}
+
+			// Fancy method to request access to account.
+			const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+		
+			// Boom! This should print out public address once we authorize Metamask.
+			console.log("Connected", accounts[0]);
+			setCurrentAccount(accounts[0]);
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+
+	const renderNotConnectedContainer = () => (
+		<div className="connect-wallet-container">
+			<img src="https://media.giphy.com/media/3ohhwytHcusSCXXOUg/giphy.gif" alt="Ninja gif" />
+			<button onClick={connectWallet} className="cta-button connect-wallet-button">
+				Connect Wallet
+			</button>
+		</div>
+  	);
+
+	useEffect(() => {
+		checkIfWalletIsConnected();
+	}, [])
 
   return (
 		<div className="App">
@@ -17,6 +81,8 @@ const App = () => {
             </div>
 					</header>
 				</div>
+
+				{renderNotConnectedContainer()}
 
         <div className="footer-container">
 					<div
