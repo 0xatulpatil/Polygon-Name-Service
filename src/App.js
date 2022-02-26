148550b4a808f923097043bbca1daaea0b87896e
Atul Patil <atuluttampatil.1757@gmail.com>
2022-02-26 23:52:23 +0530

    🛠: Add form inputs

--- src/App.js
+++ src/App.js
@@ -55,6 +55,40 @@ const App = () => {
 		}
 	}
 
+	//function to render form inputs
+	const renderInputForm = () =>{
+		return (
+			<div className="form-container">
+				<div className="first-row">
+					<input
+						type="text"
+						value={domain}
+						placeholder='domain'
+						onChange={e => setDomain(e.target.value)}
+					/>
+					<p className='tld'> {_tld} </p>
+				</div>
+
+				<input
+					type="text"
+					value={record}
+					placeholder='whats ur ninja power'
+					onChange={e => setRecord(e.target.value)}
+				/>
+
+				<div className="button-container">
+					<button className='cta-button mint-button' disabled={null} onClick={null}>
+						Mint
+					</button>  
+					<button className='cta-button mint-button' disabled={null} onClick={null}>
+						Set data
+					</button>  
+				</div>
+
+			</div>
+		);
+	}
+
 
 	const renderNotConnectedContainer = () => (
 		<div className="connect-wallet-container">
@@ -82,7 +116,9 @@ const App = () => {
 					</header>
 				</div>
 
-				{renderNotConnectedContainer()}
+				{!currentAccount && renderNotConnectedContainer()}
+				{/* Render the input form if an account is connected */}
+				{currentAccount && renderInputForm()}
 
         <div className="footer-container">
 					<div
